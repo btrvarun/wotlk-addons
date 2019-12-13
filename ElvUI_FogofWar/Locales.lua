05ea75d639664666df2efd7d46d3362773e26e50
Varun Selvakumar <btr.varun@gmail.com>
2019-12-13 23:01:10 +0530

    auto-update

---
+++ ElvUI_FogofWar/Locales.lua
@@ -0,0 +1,65 @@
+-- English
+local L = LibStub("AceLocale-3.0-ElvUI"):NewLocale("ElvUI", "enUS", true);
+
+if not L then return end
+
+L["Fog of War"] = true;
+L["Overlay Color"] = true;
+
+if GetLocale() == "enUS" then return end
+
+-- German
+local L = LibStub("AceLocale-3.0-ElvUI"):NewLocale("ElvUI", "deDE")
+if L then
+	L["Fog of War"] = "Nebel des Krieges"
+	L["Overlay Color"] = true;
+end
+
+-- Spanish
+local L = LibStub("AceLocale-3.0-ElvUI"):NewLocale("ElvUI", "esES")
+if L then
+	L["Fog of War"] = true;
+	L["Overlay Color"] = true;
+end
+
+-- French
+local L = LibStub("AceLocale-3.0-ElvUI"):NewLocale("ElvUI", "frFR")
+if L then
+	L["Fog of War"] = true;
+	L["Overlay Color"] = true;
+end
+
+-- Korean
+local L = LibStub("AceLocale-3.0-ElvUI"):NewLocale("ElvUI", "koKR")
+if L then
+	L["Fog of War"] = true;
+	L["Overlay Color"] = true;
+end
+
+-- Portuguese
+local L = LibStub("AceLocale-3.0-ElvUI"):NewLocale("ElvUI", "ptBR")
+if L then
+	L["Fog of War"] = true;
+	L["Overlay Color"] = true;
+end
+
+-- Russian
+local L = LibStub("AceLocale-3.0-ElvUI"):NewLocale("ElvUI", "ruRU")
+if L then
+	L["Fog of War"] = true;
+	L["Overlay Color"] = true;
+end
+
+--Chinese (China, simplified)
+local L = LibStub("AceLocale-3.0-ElvUI"):NewLocale("ElvUI", "zhCN")
+if L then
+	L["Fog of War"] = true;
+	L["Overlay Color"] = true;
+end
+
+--Chinese (Taiwan, traditional)
+local L = LibStub("AceLocale-3.0-ElvUI"):NewLocale("ElvUI", "zhTW")
+if L then
+	L["Fog of War"] = true;
+	L["Overlay Color"] = true;
+end
