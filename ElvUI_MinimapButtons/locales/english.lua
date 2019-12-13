ea190b847d67f5865454da031300c5fba4f2095c
Varun Selvakumar <btr.varun@gmail.com>
2019-12-14 00:01:10 +0530

    auto-update

--- ElvUI_MinimapButtons/locales/english.lua
+++
@@ -1,6 +0,0 @@
-﻿local AceLocale = LibStub:GetLibrary("AceLocale-3.0-ElvUI");
-local L = AceLocale:NewLocale("ElvUI", "enUS", true);
-if not L then return; end
-
-L["Inside Minimap"] = true;
-L["Minimap Button Grabber"] = true;
